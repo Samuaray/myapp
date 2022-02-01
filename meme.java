9df74f757d3c381a26655e3974729fca50d43c6a
Samuaray <98791636+Samuaray@users.noreply.github.com>
2022-01-31 23:11:17 -0800

    Update meme.java

--- meme.java
+++ meme.java
@@ -4,13 +4,13 @@ public class meme
     public static void main(String args[]){
 
 
-    
+   int suit = 20; 
     System.out.println("I create memes for reddit");
     System.out.println("These memes are funny");
 
       System.out.println("I currently have 5 lawsuits right now, because my memes break copyright laws");
-
-
+        
+        System.out.println("I currently have " + suit + " law suits right now");
 
 
 
